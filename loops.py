e0eb1434cdacca7e3acd434e2253deea3c1d0bca
Devesh <deveshchandra187@gmail.com>
2025-01-28 11:23:59 +0530

    updates

---
+++ loops.py
@@ -0,0 +1,130 @@
+# # factorial of a number
+# print("Factorial of a number")
+# x = int(input("Enter a number: "))
+#
+# for i in range(1, x):
+#     x = x * i
+# print(x)
+#
+# # armstrong number
+# print("Armstrong number")
+# x = int(input("Enter a number: "))
+# temp = x
+# result = 0
+# n = len(str(x))
+# while temp > 0:
+#     digit = temp % 10
+#     result += digit**n
+#     temp = temp // 10
+# if result == x:
+#     print("Armstrong number")
+# else:
+#     print("Not an armstrong number")
+# # fibonacci series
+#
+# print("Fibonacci series")
+#
+#
+# def fibonacci(n):
+#     a = 0
+#     b = 1
+#     print(a, end="\t")
+#     print(b, end="\t")
+#     for i in range(2, n):
+#         c = a + b
+#         a = b
+#         b = c
+#         print(c, end="\t")
+#
+#
+# n = int(input("Enter a number: "))
+# fibonacci(n)
+# # prime number
+# print("\nPrime number")
+# x = int(input("Enter a number: "))
+# if x > 1:
+#     for i in range(2, x):
+#         if x % i == 0:
+#             print("Not a prime number")
+#             break
+#     else:
+#         print("Prime number")
+# else:
+#     print("Not a prime number")
+
+# sum of all digits
+print("Sum of all digits")
+x = int(input("Enter a number: "))
+sum = 0
+while x > 0:
+    digit = x % 10
+    sum += digit
+    x = x // 10
+print(sum)
+#
+#
+# # palindrome number
+# print("Palindrome number")
+#
+#
+# def palindrome(n):
+#     temp = n
+#     rev = 0
+#     while n > 0:
+#         digit = n % 10
+#         rev = rev * 10 + digit
+#         n = n // 10
+#     if temp == rev:
+#         print("Palindrome number")
+#     else:
+#         print("Not a palindrome number")
+#
+#
+# n = int(input("Enter a number: "))
+# palindrome(n)
+#
+# # count and print all the numbers divisible by 5 or 7 between 1 and 100
+# print("Numbers divisible by 5 or 7")
+#
+#
+# def divisible():
+#     for i in range(1, 101):
+#         if i % 5 == 0 or i % 7 == 0:
+#             print(i)
+#
+#
+# divisible()
+#
+# # covert all lowercase to upper case in a string using loop
+# print("Convert lowercase to uppercase")
+# string = input("Enter a string: ")
+# new_string = ""
+# for i in string:
+#     if i.islower():
+#         new_string += i.upper()
+#     else:
+#         new_string += i
+# print(new_string)
+#
+# # print all prime numbers between 1 and 100
+# print("Prime numbers between 1 and 100")
+# for i in range(1, 101):
+#     if i > 1:
+#         for j in range(2, i):
+#             if i % j == 0:
+#                 break
+#         else:
+#             print(i)
+#
+# # print the times table for a given number
+#
+# print("Times table")
+#
+#
+# def table(n):
+#     for i in range(1, 11):
+#         print(n, "x", i, "=", n * i)
+#
+#
+# n = int(input("Enter a number: "))
+# table(n)
--- tuple.py
+++ tuple.py
@@ -1,34 +1,128 @@
-tuple1 = (1, 2, 3, 4, 5)
-tuple2 = (6, 7, 8, 9, 10)
+print("1. Create a tuple and perform operations")
+test_tuple = (1, 2, 3, 4, 5)
 
-tuple3 = tuple1 + tuple2
+print("Traversal:")
+for item in test_tuple:
+    print(item)
+
+print("\nLength of test_tuple:")
+print(len(test_tuple))
+
+print("\nConcatenation of test_tuple and test_tuple2:")
+test_tuple2 = (6, 7, 8)
+concatenated_test_tuple = test_tuple + test_tuple2
+print(concatenated_test_tuple)
+
+print("\nRepetition of test_tuple (repeated twice):")
+repeated_test_tuple = test_tuple * 2
+print(repeated_test_tuple)
+
+print("\nSlicing test_tuple (from index 1 to 4):")
+sliced_test_tuple = test_tuple[1:4]
+print(sliced_test_tuple)
+
+print("\nCheck if 3 is in test_tuple and if 6 is in test_tuple:")
+print(3 in test_tuple)
+print(6 in test_tuple)
+
+print("\nIteration over test_tuple:")
+for i in test_tuple:
+    print(i)
+
+print("\nCompare test_tuple and test_tuple2:")
+print(test_tuple == test_tuple2)
+
+print("\nMaximum and Minimum values in test_tuple:")
+print(max(test_tuple))
+print(min(test_tuple))
+
+print("\nConvert a list to a tuple:")
+test_tuple3 = tuple([10, 20, 30])
+print(test_tuple3)
+
+print("\nTuple assignment to variables:")
+a, b, c, d, e = test_tuple
+print(a, b, c, d, e)
+
+print("\nReturning multiple values using a tuple:")
 
 
-print(tuple3)
-print(tuple3[3:9])
-
-del tuple3
-try:
-    print(tuple3)
-except:
-    print("Tuple is deleted")
+def return_test_tuple():
+    return 1, 2, 3
 
 
-tuple_to_set = set(tuple1)
-print(tuple_to_set)
-set_to_tuple = tuple(tuple_to_set)
-print(set_to_tuple)
-print(tuple1 < tuple2)
+a, b, c = return_test_tuple()
+print(a, b, c)
 
-unsorted = (1, 213, 132, 123, 13, 12321, 3)
-sorted_tuple = tuple(sorted(unsorted))
-print(sorted_tuple)
-print(tuple("HELLO"))
+print("\nNested tuple and print its elements:")
+test_tuple4 = (1, (2, 3), (4, 5))
+for item in test_tuple4:
+    print(item)
 
-print("Tuple Assigment")
-del tuple1
-tuple1 = (1, 2, 3)
+print("\nIndex of element 3 in test_tuple:")
+print(test_tuple.index(3))
 
-val1, val2, val3 = tuple1
+print("\nCount of element 2 in test_tuple:")
+print(test_tuple.count(2))
 
-print(val1, val2, val3)
+print("\nList comprehension using test_tuple:")
+new_test_tuple = tuple(i * 2 for i in test_tuple)
+print(new_test_tuple)
+
+print("\nVariable-length argument tuples:")
+
+
+def variable_length_args(*args):
+    print(args)
+
+
+variable_length_args(1, 2, 3, 4)
+
+print("\nZip function with two tuples:")
+test_tuple5 = (1, 2, 3)
+test_tuple6 = ("a", "b", "c")
+zipped = zip(test_tuple5, test_tuple6)
+print(list(zipped))
+
+# 2. Create a tuple with different data types
+print("\n2. Create a tuple with different data types")
+test_tuple7 = (1, "hello", 3.14, True)
+print(test_tuple7)
+
+# 3. Unpack a tuple into several variables
+print("\n3. Unpack a tuple into several variables")
+test_tuple8 = (10, 20, 30)
+a, b, c = test_tuple8
+print(a, b, c)
+
+# 4. Add an item to a tuple (create a new one)
+print("\n4. Add an item to a tuple (create a new one)")
+test_tuple = (1, 2, 3)
+new_item = (4,)
+test_tuple = test_tuple + new_item
+print(test_tuple)
+
+# 5. Convert a tuple to a string
+print("\n5. Convert a tuple to a string")
+test_tuple9 = ("a", "b", "c")
+string = "".join(test_tuple9)
+print(string)
+
+# 6. Find repeated items in a tuple
+print("\n6. Find repeated items in a tuple")
+test_tuple10 = (1, 2, 3, 4, 2, 5, 3)
+repeated_items = [item for item in set(test_tuple10) if test_tuple10.count(item) > 1]
+print(repeated_items)
+
+# 7. Check whether an element exists within a tuple
+print("\n7. Check whether an element exists within a tuple")
+test_tuple11 = (1, 2, 3, 4)
+print(3 in test_tuple11)
+print(5 in test_tuple11)
+
+# 8. Unzip a list of tuples into individual lists
+print("\n8. Unzip a list of tuples into individual lists")
+list_of_test_tuples = [(1, "a"), (2, "b"), (3, "c")]
+unzipped = zip(*list_of_test_tuples)
+unzipped_list = [list(item) for item in unzipped]
+print(unzipped_list)
